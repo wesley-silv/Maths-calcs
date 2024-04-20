ed591b8ffd537eeee3d02875bec087897c09f1ce
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-04-20 06:48:06 -0300

    Template of triangle area complete.

---
+++ main.js
@@ -0,0 +1,18 @@
+let triagleArea = document.getElementById('result-triangle')
+
+triagleArea.addEventListener('click', triangle)
+
+function triangle() {
+  let base = document.getElementById('base-triangle').value
+  let height = document.getElementById('height-triangle').value
+  let area = (base * height) / 2
+  if (area <= 0) {
+    let showArea = (document.getElementById(
+      'show'
+    ).innerHTML = `Preencha os capos para obter o valor da área em m²`)
+  } else {
+    let showArea = (document.getElementById(
+      'show'
+    ).innerHTML = `A área do triângulo é igual: ${area} m²`)
+  }
+}
