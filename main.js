f265944e140508eed3d6711cb42cae7886dab4a5
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-05-01 18:11:59 -0300

    Logic of project complete to cards that was created.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let triagleArea = document.getElementById('result-triangle')
-
 triagleArea.addEventListener('click', triangle)
 
 function triangle() {
@@ -14,5 +13,58 @@ function triangle() {
     let showArea = (document.getElementById(
       'show'
     ).innerHTML = `A área do triângulo é igual: ${area} m²`)
+  }
+}
+
+let circleArea = document.getElementById('result-circle')
+circleArea.addEventListener('click', circle)
+
+function circle() {
+  let pi = document.getElementById('pi-circle').value
+  let ray = document.getElementById('ray-circle').value
+  let area = pi * (ray * ray)
+  if (area <= 0) {
+    let showArea = (document.getElementById(
+      'circle-show'
+    ).innerHTML = `Preencha os campos para obter o valor da área em m²`)
+  } else {
+    let showArea = (document.getElementById(
+      'circle-show'
+    ).innerHTML = `A área do circule é igual: ${area} m²`)
+  }
+}
+
+let squareArea = document.getElementById('result-square')
+squareArea.addEventListener('click', square)
+
+function square() {
+  let base = document.getElementById('base-square').value
+  let area = (base * base)
+  if (area <= 0) {
+    let showArea = (document.getElementById(
+      'square-show'
+    ).innerHTML = `Preencha os campos para obter o valor da área em m²`)
+  } else {
+    let showArea = (document.getElementById(
+      'square-show'
+    ).innerHTML = `A área do quadrado é igual: ${area} m²`)
+  }
+}
+
+let mediaArea = document.getElementById('result-media')
+mediaArea.addEventListener('click', media)
+
+function media() {
+  let totalVelues = document.getElementById('values').value
+  let quantities = document.getElementById('quantities').value
+  let media = totalVelues / quantities
+  if (media <= 0) {
+    let showArea = (document.getElementById(
+      'media-show'
+    ).innerHTML = `Preencha os campos para obter o valor da média`)
+  } else {
+    let showArea = (document.getElementById(
+      'media-show'
+    ).innerHTML = `O valor da Média é igual: ${media}`)
   }
 }
