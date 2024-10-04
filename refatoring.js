97b5d1a7e2c47b90ccb682e4c441e811d69fbb36
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-10-03 23:01:36 -0300

    Refatorando a apresentação de casas deciamais nos resultados, disposição do código javascript conforme a disposição dos cards no html.

--- refatoring.js
+++ refatoring.js
@@ -1,3 +1,81 @@
+document.getElementById('result-media').addEventListener('click', function () {
+  const values = parseFloat(document.getElementById('values').value)
+  const quantities = parseFloat(document.getElementById('quantities').value)
+
+  if (isNaN(values) || isNaN(quantities)) {
+    alert('Insira valores numéricos válidos!')
+    return
+  }
+  const mean = values / quantities
+  document.getElementById('media-show').innerText = `Média: ${mean.toFixed(2)}`
+})
+
+document
+  .getElementById('result-percent')
+  .addEventListener('click', function () {
+    const value = parseFloat(document.getElementById('value').value)
+    const percentValue = parseFloat(document.getElementById('percent').value)
+
+    if (isNaN(value) || isNaN(percentValue)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
+    const percent = (value / 100) * percentValue
+    document.getElementById(
+      'percent-show'
+    ).innerText = `Porcentagem: ${percent.toFixed(2)}`
+  })
+
+document
+  .getElementById('result-percent-ratio')
+  .addEventListener('click', function () {
+    const valueRatio = parseFloat(document.getElementById('value-ratio').value)
+    const percentRatio = parseFloat(
+      document.getElementById('percent-ratio').value
+    )
+
+    if (isNaN(valueRatio) || isNaN(percentRatio)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
+    const percent = (percentRatio / valueRatio) * 100
+    document.getElementById(
+      'percent-ratio-show'
+    ).innerText = `Relação percentual: ${percent.toFixed(2)}%`
+  })
+
+document.getElementById('result-imc').addEventListener('click', function () {
+  const mass = parseFloat(document.getElementById('mass').value)
+  const height = parseFloat(document.getElementById('height').value)
+
+  if (isNaN(mass) || isNaN(height)) {
+    alert('Insira valores numéricos válidos!')
+    return
+  }
+  const imc = mass / Math.pow(height, 2)
+  document.getElementById('imc-show').innerText = `IMC: ${imc.toFixed(2)}`
+})
+
+document
+  .getElementById('result-risk-analysis')
+  .addEventListener('click', function () {
+    const amount = parseFloat(document.getElementById('amount').value)
+    const margin = parseFloat(document.getElementById('margin').value)
+    const units = parseFloat(document.getElementById('units').value)
+
+    if (isNaN(amount) || isNaN(margin) || isNaN(units)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
+    const riskMargin = (amount * (margin / 100)) / units
+    document.getElementById(
+      'risk-analysis-show'
+    ).innerText = `Margem de risco: R$ ${riskMargin.toFixed(2)}`
+    document.getElementById('risk-analysis-total').innerText = `Perda: R$ ${
+      riskMargin.toFixed(2) * units
+    }`
+  })
+
 document
   .getElementById('result-triangle')
   .addEventListener('click', function () {
@@ -34,81 +112,3 @@ document.getElementById('result-square').addEventListener('click', function () {
   const area = Math.pow(base, 2)
   document.getElementById('square-show').innerText = `Área: ${area}`
 })
-
-document.getElementById('result-media').addEventListener('click', function () {
-  const values = parseFloat(document.getElementById('values').value)
-  const quantities = parseFloat(document.getElementById('quantities').value)
-
-  if (isNaN(values) || isNaN(quantities)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
-  const mean = values / quantities
-  document.getElementById('media-show').innerText = `Média: ${mean}`
-})
-
-document.getElementById('result-imc').addEventListener('click', function () {
-  const mass = parseFloat(document.getElementById('mass').value)
-  const height = parseFloat(document.getElementById('height').value)
-
-  if (isNaN(mass) || isNaN(height)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
-  const imc = mass / Math.pow(height, 2)
-  document.getElementById('imc-show').innerText = `IMC: ${imc}`
-})
-
-document
-  .getElementById('result-percent')
-  .addEventListener('click', function () {
-    const value = parseFloat(document.getElementById('value').value)
-    const percentValue = parseFloat(document.getElementById('percent').value)
-
-    if (isNaN(value) || isNaN(percentValue)) {
-      alert('Insira valores numéricos válidos!')
-      return
-    }
-    const percent = (value / 100) * percentValue
-    document.getElementById(
-      'percent-show'
-    ).innerText = `Porcentagem: ${percent}`
-  })
-
-document
-  .getElementById('result-percent-ratio')
-  .addEventListener('click', function () {
-    const valueRatio = parseFloat(document.getElementById('value-ratio').value)
-    const percentRatio = parseFloat(
-      document.getElementById('percent-ratio').value
-    )
-
-    if (isNaN(valueRatio) || isNaN(percentRatio)) {
-      alert('Insira valores numéricos válidos!')
-      return
-    }
-    const percent = (percentRatio / valueRatio) * 100
-    document.getElementById(
-      'percent-ratio-show'
-    ).innerText = `Relação percentual: ${percent} %`
-  })
-
-document
-  .getElementById('result-risk-analysis')
-  .addEventListener('click', function () {
-    const amount = parseFloat(document.getElementById('amount').value)
-    const margin = parseFloat(document.getElementById('margin').value)
-    const units = parseFloat(document.getElementById('units').value)
-
-    if (isNaN(amount) || isNaN(margin) || isNaN(units)) {
-      alert('Insira valores numéricos válidos!')
-      return
-    }
-    const riskMargin = (amount * (margin / 100)) / units
-    document.getElementById(
-      'risk-analysis-show'
-    ).innerText = `Margem de risco: R$ ${riskMargin}`
-    document.getElementById('risk-analysis-total').innerText = `Perda: R$ ${
-      riskMargin * units
-    }`
-  })
