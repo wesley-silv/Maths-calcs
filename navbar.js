ff17dc30ecb03bb4b6bfdf0095adfe8784a586e4
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-10-07 19:20:12 -0300

    Refatorando o html do projeto, retirando itens desnecessários.

--- navbar.js
+++ navbar.js
@@ -1,10 +1,8 @@
-// Função para abrir e fechar a navbar
 function toggleNavbar() {
   const nav = document.querySelector('nav')
   nav.classList.toggle('nav-open')
 }
 
-// Verifica se a largura da tela mudou para maior que 768px e reseta o estado do menu
 window.addEventListener('resize', function () {
   const nav = document.querySelector('nav')
   if (window.innerWidth >= 768) {
