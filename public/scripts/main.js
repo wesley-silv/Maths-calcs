bc3263f34bc1897ffa98dd0b6cf4145c5190a30b
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-10-24 18:35:53 -0300

    refactor(main.js): adding a new format to identifier each functionality of project scope

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,7 +11,7 @@ function registerOperation(description, value, quantity, result) {
   realizedOperations.push(operation)
 }
 
-document
+const downloadOptions = document
   .getElementById('download-options')
   .addEventListener('change', function () {
     const selectedOption = this.value
@@ -78,22 +78,26 @@ function downloadXLSX() {
   XLSX.writeFile(wb, 'Operations-file.xlsx')
 }
 
-document.getElementById('result-media').addEventListener('click', function () {
-  const values = parseFloat(document.getElementById('values').value)
-  const quantities = parseFloat(document.getElementById('quantities').value)
+const mediaCalc = document
+  .getElementById('result-media')
+  .addEventListener('click', function () {
+    const values = parseFloat(document.getElementById('values').value)
+    const quantities = parseFloat(document.getElementById('quantities').value)
 
-  if (isNaN(values) || isNaN(quantities)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
+    if (isNaN(values) || isNaN(quantities)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
 
-  const mean = values / quantities
-  document.getElementById('media-show').innerText = `Média: ${mean.toFixed(2)}`
+    const mean = values / quantities
+    document.getElementById('media-show').innerText = `Média: ${mean.toFixed(
+      2
+    )}`
 
-  registerOperation('Média Aritmética', values, quantities, mean.toFixed(2))
-})
+    registerOperation('Média Aritmética', values, quantities, mean.toFixed(2))
+  })
 
-document
+const percentCalc = document
   .getElementById('result-percent')
   .addEventListener('click', function () {
     const value = parseFloat(document.getElementById('value').value)
@@ -112,7 +116,7 @@ document
     registerOperation('Porcentagem', value, percentValue, percent.toFixed(2))
   })
 
-document
+const percentRatioCalc = document
   .getElementById('result-percent-ratio')
   .addEventListener('click', function () {
     const valueRatio = parseFloat(document.getElementById('value-ratio').value)
@@ -138,22 +142,24 @@ document
     )
   })
 
-document.getElementById('result-imc').addEventListener('click', function () {
-  const mass = parseFloat(document.getElementById('mass').value)
-  const height = parseFloat(document.getElementById('height').value)
+const imcCalc = document
+  .getElementById('result-imc')
+  .addEventListener('click', function () {
+    const mass = parseFloat(document.getElementById('mass').value)
+    const height = parseFloat(document.getElementById('height').value)
 
-  if (isNaN(mass) || isNaN(height)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
+    if (isNaN(mass) || isNaN(height)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
 
-  const imc = mass / Math.pow(height, 2)
-  document.getElementById('imc-show').innerText = `IMC: ${imc.toFixed(2)}`
+    const imc = mass / Math.pow(height, 2)
+    document.getElementById('imc-show').innerText = `IMC: ${imc.toFixed(2)}`
 
-  registerOperation('IMC', mass, height, imc.toFixed(2))
-})
+    registerOperation('IMC', mass, height, imc.toFixed(2))
+  })
 
-document
+const risckAnalysisCalc = document
   .getElementById('result-risk-analysis')
   .addEventListener('click', function () {
     const amount = parseFloat(document.getElementById('amount').value)
@@ -181,7 +187,7 @@ document
     )
   })
 
-document
+const triangleCalc = document
   .getElementById('result-triangle')
   .addEventListener('click', function () {
     const base = parseFloat(document.getElementById('base-triangle').value)
@@ -198,31 +204,39 @@ document
     registerOperation('Área do Triângulo', base, height, area.toFixed(2))
   })
 
-document.getElementById('result-circle').addEventListener('click', function () {
-  const pi = parseFloat(document.getElementById('pi-circle').value)
-  const radius = parseFloat(document.getElementById('ray-circle').value)
+const circleCalc = document
+  .getElementById('result-circle')
+  .addEventListener('click', function () {
+    const pi = parseFloat(document.getElementById('pi-circle').value)
+    const radius = parseFloat(document.getElementById('ray-circle').value)
 
-  if (isNaN(pi) || isNaN(radius)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
+    if (isNaN(pi) || isNaN(radius)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
 
-  const area = pi * Math.pow(radius, 2)
-  document.getElementById('circle-show').innerText = `Área: ${area.toFixed(2)}`
+    const area = pi * Math.pow(radius, 2)
+    document.getElementById('circle-show').innerText = `Área: ${area.toFixed(
+      2
+    )}`
 
-  registerOperation('Área do Círculo', pi, radius, area.toFixed(2))
-})
+    registerOperation('Área do Círculo', pi, radius, area.toFixed(2))
+  })
 
-document.getElementById('result-square').addEventListener('click', function () {
-  const base = parseFloat(document.getElementById('base-square').value)
+const squareCalc = document
+  .getElementById('result-square')
+  .addEventListener('click', function () {
+    const base = parseFloat(document.getElementById('base-square').value)
 
-  if (isNaN(base)) {
-    alert('Insira valores numéricos válidos!')
-    return
-  }
+    if (isNaN(base)) {
+      alert('Insira valores numéricos válidos!')
+      return
+    }
 
-  const area = Math.pow(base, 2)
-  document.getElementById('square-show').innerText = `Área: ${area.toFixed(2)}`
+    const area = Math.pow(base, 2)
+    document.getElementById('square-show').innerText = `Área: ${area.toFixed(
+      2
+    )}`
 
-  registerOperation('Área do Quadrado', base, base, area.toFixed(2))
-})
+    registerOperation('Área do Quadrado', base, base, area.toFixed(2))
+  })
