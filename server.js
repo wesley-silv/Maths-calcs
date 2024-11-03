40cda8886a23bd50dc5ea736e1129a2cf0c09f56
Wesley Silva <wesleysilvaconceicao@outlook.com>
2024-11-03 14:07:23 -0300

    feat(main.js, server.js quotes.js, quotes.html and quotes.css): was added a new function to cotation
    This implementation is used a API request to found the cotation of ticker rightely of B3.

--- server.js
+++ server.js
@@ -9,6 +9,35 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+app.get('/api/cotacoes', async (req, res) => {
+  try {
+    const fetch = await import('node-fetch') // Importação dinâmica
+    const response = await fetch.default('https://brapi.dev/api/quote/mxrf11', {
+      headers: {
+        Authorization: 'avbwNQ679JViRzR3inZxLY' // Insira o token aqui
+      }
+    })
+    const data = await response.json()
+
+    if (data.results && Array.isArray(data.results)) {
+      const cotacoes = data.results.map(cotacao => ({
+        nome: cotacao.symbol,
+        preco: cotacao.regularMarketPrice,
+        variacao: cotacao.regularMarketChangePercent
+      }))
+      res.json(cotacoes)
+    } else {
+      console.error('Estrutura de dados inesperada:', data)
+      res.status(500).json({
+        message: 'Erro ao processar as cotações: estrutura de dados inesperada'
+      })
+    }
+  } catch (error) {
+    console.error('Erro ao obter as cotações:', error)
+    res.status(500).json({ message: 'Erro ao obter as cotações' })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}/`)
 })
